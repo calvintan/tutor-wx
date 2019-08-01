d80e5b86626a8a1de6e1e884a435b08462b8c15a
Calvin Tan <hello@calvin-tan.com>
2019-08-01 19:21:10 +0800

    Restyle pages

--- pages/show/show.js
+++ pages/show/show.js
@@ -18,8 +18,8 @@ Page({
       url: `http://localhost:3000/api/v1/services/${id}`,
       // url: `https://tutor-app-mp.herokuapp.com/api/v1/services/${id}`,
       success: function (res) {
-        console.log(res.data.service);
-        const service = res.data.service;
+        console.log(res.data);
+        const service = res.data;
 
         // Update local data
         page.setData({
