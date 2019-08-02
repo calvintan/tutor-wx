7eda9e4f6e3940316cdb12c68769371e0e7e7391
Yutian Han <kevinhangoat@gmail.com>
2019-08-02 11:09:48 +0800

    user edit and delete on profile page

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -6,7 +6,7 @@ const months = []
 const days = []
 const times = []
 
-for (let i = date.getFullYear() ; i <= date.getFullYear() + 3; i++) {
+for (let i = date.getFullYear(); i <= date.getFullYear() + 3; i++) {
   years.push(i)
 }
 
@@ -14,7 +14,7 @@ for (let i = 1; i <= 12; i++) {
   months.push(i)
 }
 
-for (let i = 1 ; i <= 31; i++) {
+for (let i = 1; i <= 31; i++) {
   days.push(i)
 }
 
@@ -70,6 +70,28 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    let page = this;
+    console.log('options', options)
+    wx.showToast({
+      title: 'Loading...',
+      icon: 'loading',
+      duration: 1500
+    });
+    // Get story data from server (to show in form)
+    wx.request({
+      url: `http://localhost:3000/api/v1/services/${options.id}`,
+      success(res) {
+        console.log('res',res)
+        var service = res.data.service;
+
+        // Update local data
+        page.setData({
+          service: service
+        });
+
+        wx.hideToast();
+      }
+    });
 
   },
 
@@ -122,47 +144,49 @@ Page({
 
   },
   formSubmit: function (e) {
-    wx.authorize({
-      scope: 'scope.userInfo',
-      success: res => {
-        console.log('res', res)
-        wx.getUserInfo({
-          success: function (res) {
-            var userInfo = res.userInfo
-            var nickName = userInfo.nickName
-            var avatarUrl = userInfo.avatarUrl
-            var gender = userInfo.gender //性别 0：未知、1：男、2：女
-            var province = userInfo.province
-            var city = userInfo.city
-            var country = userInfo.country
-            console.log(nickName)
-          }
-        })
-      },
-      fail: f => {
-        console.log('f',f)
-      }
-    })
+    // wx.authorize({
+    //   scope: 'scope.userInfo',
+    //   success: res => {
+    //     console.log('res', res)
+    //     wx.getUserInfo({
+    //       success: function (res) {
+    //         var userInfo = res.userInfo
+    //         var nickName = userInfo.nickName
+    //         var avatarUrl = userInfo.avatarUrl
+    //         var gender = userInfo.gender //性别 0：未知、1：男、2：女
+    //         var province = userInfo.province
+    //         var city = userInfo.city
+    //         var country = userInfo.country
+    //         console.log(nickName)
+    //       }
+    //     })
+    //   },
+    //   fail: f => {
+    //     console.log('f', f)
+    //   }
+    // })
 
-    console.log('scope',this.scope)
+    //console.log('scope', this.scope)
     let title = e.detail.value.title
     let description = e.detail.value.description
-    let category = e.detail.value.category
+    let category = this.data.service.category
     let difficulty = e.detail.value.difficulty
     let location = e.detail.value.location
     let saved_date = [this.data.year, this.data.month, this.data.day]
-    let event = { title: title, user_id: getApp().globalData.userId, description: description, category: category, description: description, location: location, difficulty: difficulty, time: new Date(this.data.year, this.data.month - 1, this.data.day, this.data.time)}
-    console.log("event",event)
+    console.log('e',e)
+    let id = getApp()
+    let event = { title: title, user_id: getApp().globalData.userId, description: description, category: category, description: description, location: location, difficulty: difficulty, time: new Date(this.data.year, this.data.month - 1, this.data.day, this.data.time) }
+    console.log("event", event)
+    console.log("service",this.data.service)
     wx.request({
-      url: 'http://localhost:3000/api/v1/services/',
-      // url: 'https://tutor-app-mp.herokuapp.com/api/v1/services/',
-      method: "POST",
+      url: `http://localhost:3000/api/v1/services/${this.data.service.id} `,
+      // url: 'https://tutor-app-mp.herokuapp.com/api/v1/services/${id}',
+      method: "PUT",
       data: event,
       success() {
         wx.reLaunch({
           url: '/pages/profiles/profile',
         })
-        console.log('success')
       }
     })
   },
